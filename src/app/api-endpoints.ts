12f1f5f4b0ff3ad4f74decb79d2729da5a9c42d2
chawanakshata <90598127+chawanakshata@users.noreply.github.com>
2025-05-13 18:18:56 +0530

    Update api-endpoints.ts

--- src/app/api-endpoints.ts
+++ src/app/api-endpoints.ts
@@ -3,7 +3,8 @@ export const API_ENDPOINTS = {
   USERS: 'https://localhost:7085/api/Users',
   USER_FACTS: 'https://localhost:7085/api/Users/facts',
   USER_FACTS_UPDATE: (factId: number) => `https://localhost:7085/api/Users/facts/${factId}`,
-  
+  USER_FACTS_DELETE: (factId: number) => `https://localhost:7085/api/Users/facts/${factId}`,
+
   TRAINING_ACTIVITIES: 'https://localhost:7085/api/TrainingActivities',
 
   TEAM_SELFIES: 'https://localhost:7085/api/TeamSelfies',
